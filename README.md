58c4e3b26d51629074c3dea129707d7dd3c2c274
RP <rp@g.com>
2020-07-05 19:27:32 +0530

    post app

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+# Command to generate release
+flutter build apk
+
+# enable internet connection
+https://stackoverflow.com/questions/60076098/flutter-app-working-on-emulator-but-not-on-device-uses-http-package
+# https://www.youtube.com/watch?v=1b06d2Ld8C4&feature=youtu.be
---
+++ lib/homepage.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+
+import 'package:awesome/detailpage.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'appconstant.dart';
+
+class HomePage extends StatefulWidget{
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //var apiEndpoint = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
+  var apiUrl = "https://jsonplaceholder.typicode.com/posts";
+  var apiRes, posts;
+
+  @override
+  void initState() {
+    super.initState();
+    getDataFromAPI();
+  }
+
+  getDataFromAPI() async{
+    apiRes = await http.get(apiUrl);
+    //print(apiRes.body);// this is as string response, so it has to be converted to json
+    posts = jsonDecode(apiRes.body);
+    //print(posts);
+    setState(() {
+      
+    });
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(gradient: LinearGradient(colors: [
+          appColor, Colors.orange
+        ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Post App"),
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Center(
+           child: apiRes != null ? ListView.builder( // 0
+          /*1*/ // child: apiRes != null ? GridView.builder(
+          /*2*/ // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          // uncomment 1 and 2 and comment 0 to see GridView and vice versa to see ListView 
+            itemCount: posts.length,
+            itemBuilder: (context, index){
+              var post = posts[index];
+              return ListTile(
+                leading: Hero(
+                tag: post["id"],
+                child: CircleAvatar(backgroundImage: NetworkImage("https://jssors8.azureedge.net/demos/img/gallery/720x480/006.jpg"))),
+                title: Text("${post["title"]}",
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                  ),
+                ),
+                subtitle: Text("${post["id"]}",
+                  style: TextStyle(
+                    //fontSize: 22.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onTap: (){
+                  // https://www.youtube.com/watch?v=1b06d2Ld8C4&feature=youtu.be
+                  // check 1.09 min for dialog popup
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(post: post)));
+                },
+              );
+            }
+          ) : CircularProgressIndicator(backgroundColor: Colors.white,)
+        ),
+      ),
+    );
+  }
+}
